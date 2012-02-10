c6c2e1f011fd00643b1a8e4185311186f735a55f
Joe Julian <me@joejulian.name>
2012-02-10 11:22:20 -0800

    initial checkin

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: exim
+#
+# This module manages exim
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class exim {
+
+
+}
